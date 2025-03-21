02806a3575480c1a365442a3fdd81ff16ecb616a
Manjeet <yadavmanjeet629@gmail.com>
2025-03-21 18:36:01 +0530

    add comment, like, and post models with pagination support

---
+++ backend/src/models/comment.model.js
@@ -0,0 +1,30 @@
+import mongoose, {Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: {
+            validator: function(v) {
+                return v.length > 0;
+            },
+            message: "Content cannot be empty"
+        }
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+        required: true      //remove if generic comment to be stored for various models
+    },
+    commentBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+export const Comment = mongoose.model("Comment", commentSchema)
