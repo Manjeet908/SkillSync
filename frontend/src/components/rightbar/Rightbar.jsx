41e68e8850c8b85fb4e3bbcb28cbfbc45f4ab62b
abdulkalam0018 <abdulkalam754600@gmail.com>
2025-04-11 18:43:24 +0530

    feat: enhance Rightbar component with birthday notifications and advertisements

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 import './rightbar.css'
 function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className='birthdayImg' src="/assets/gift/gift.png" alt="" />  
+          <span className="birthdayText">
+            <b>Aditya</b> and <b>3 other friends</b> have a birthday today.
+          </span>     
+        </div>
+        <img src="/assets/gift/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className='rightbarFriendList'>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/post/2.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Aditya</span>
+          </li>
+          </ul>
+
+
+      </div>
+    </div>
   )
 }
 
