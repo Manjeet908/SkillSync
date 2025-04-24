7d8eaf777ab9f1c4c58f6c0ef1a98cdcdaaf07e6
abdulkalam0018 <abdulkalam754600@gmail.com>
2025-04-25 01:21:10 +0530

    ProfileModal component

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -6,11 +6,13 @@ import Rightbar from "../../components/rightbar/Rightbar";
 import { useEffect, useState } from "react";
 import axiosInstance from "../../api/axios";
 import { useParams } from "react-router";
+import ProfileModal from "../profileModal/ProfileModal";
 
 export default function Profile() {
   const PF = import.meta.env.VITE_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({});
   const username = useParams().username;
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -29,20 +31,27 @@ export default function Profile() {
         <div className="profileRight">
           <div className="profileRightTop">
             <div className="profileCover">
-              <img
-                className="profileCoverImg"
-                src={
-                  user.coverPicture
-                }
-                alt=""
-              />
+            <img
+              className="profileCoverImg"
+              src={
+                user.coverPicture
+                  ? PF + user.coverPicture
+                  : PF + "assets/gift/background.jpg"
+              }
+              alt=""
+        
+            />
               <img
                 className="profileUserImg"
                 src={
                   user.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + "assets/person/1.jpg"
                 }
                 alt=""
+                onClick={() => setIsModalOpen(true)}
               />
+              {isModalOpen && <ProfileModal onClose={() => setIsModalOpen(false)} />}
             </div>
             <div className="profileInfo">
               <h4 className="profileInfoName">{user.username}</h4>
