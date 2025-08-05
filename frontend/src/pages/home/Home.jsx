67ffcca4e1d851515470765ff2a97e7799401a67
Manjeet Kumar <149872277+Manjeet908@users.noreply.github.com>
2025-08-05 23:31:49 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Rightbar from "../../components/rightbar/Rightbar";
 import Feed from "../../components/feed/Feed";
-import "./home.css";
+import "./Home.css";
 
 export default function Home() {
   return (
